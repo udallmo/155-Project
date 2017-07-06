5238f58789d59160989d2d7bc72e038867d2e0d7
EIivaras <zach00182@gmail.com>
2017-07-05 21:50:48 -0400

    Lab4 #5 Done

--- app/src/main/java/uwaterloo/ca/lab3_204_08/GameLoopTask.java
+++ app/src/main/java/uwaterloo/ca/lab3_204_08/GameLoopTask.java
@@ -3,7 +3,11 @@ package uwaterloo.ca.lab3_204_08;
 import android.app.Activity;
 import android.content.Context;
 import android.widget.RelativeLayout;
+
+import java.util.LinkedList;
+import java.util.Random;
 import java.util.TimerTask;
+
 
 public class GameLoopTask extends TimerTask{
 
@@ -14,12 +18,26 @@ public class GameLoopTask extends TimerTask{
     private RelativeLayout gameloopRL;
     private Context gameloopCTX;
     private Activity thisActivity;
+    private int randX, randY;
+    private boolean initialBlock = true; //If we have yet to create the initial block on the game board, this is true.
+
+    LinkedList<GameBlock> GBList = new LinkedList();
+    Random R = new Random();
+
 
 /*Creates the block and set the starting the location
 * Adds the block to the view*/
     private void createBlock(){
-        newBlock = new GameBlock(gameloopCTX, -65, -65);
+        randX = R.nextInt(815) - 65;
+        randY = R.nextInt(815) - 65;
+        if (initialBlock) {
+            newBlock = new GameBlock(gameloopCTX, -65, -65);
+            initialBlock = false;
+        } else{
+            newBlock = new GameBlock(gameloopCTX, randX, randY);
+        }
         gameloopRL.addView(newBlock);
+        GBList.add(newBlock);
     }
 
 /* Constructor for GameloopTask*/
@@ -32,7 +50,12 @@ public class GameLoopTask extends TimerTask{
     }
 
     public void setDirection(Directions currentDirection){
-            newBlock.setBlockDirection(currentDirection);
+            for(GameBlock newBlock : GBList) {
+                newBlock.setBlockDirection(currentDirection);
+            }
+            if (currentDirection != Directions.NO_MOVEMENT){ //make sure blocks are only being created on a movement
+                createBlock();
+            }
     }
 
 //Runs move method to the newBlock
@@ -40,7 +63,9 @@ public class GameLoopTask extends TimerTask{
         thisActivity.runOnUiThread(
                 new Runnable(){
                     public void run() {
-                        newBlock.move();
+                        for(GameBlock newBlock : GBList) {
+                            newBlock.move();
+                        }
                     }
                 }
         );
